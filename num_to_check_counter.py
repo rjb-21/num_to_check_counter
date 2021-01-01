5df3bf21ed7cea4b29616e6e724f41c6f132db8d
Radoslaw <71033043+rjb-21@users.noreply.github.com>
2021-01-01 20:14:25 +0100

    First commit

---
+++ num_to_check_counter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+n1 = 372**2
+n2 = 809**2
+numbers = 0
+for i in range(n1,n2):
+    i_str = str(i)
+    if i_str[0] <= i_str[1] and i_str[1] <= i_str[2] and i_str[2] <= i_str[3] and i_str[3] <= i_str[4] and i_str[4] <= i_str[5]:
+        pairs = []
+        counter = 0
+        for j in range(0,5):
+            if i_str[j] == i_str[j+1]:
+                counter += 1
+                pairs.append(i_str[j]+i_str[j+1])
+                if counter > 1:
+                    pairs_set = set(pairs)
+                    if len(pairs_set) > 1:
+                        print(i)
+                        numbers += 1
+print(f'There is {numbers} numbers to check.')                
+            
+
+
+# In[53]:
+
+
+'i_str[j]'+'i_str[j+1]'
+
+
+# In[ ]:
+
+
+
+
